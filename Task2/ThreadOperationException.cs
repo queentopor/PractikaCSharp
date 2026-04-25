0a2858e07c89c72dd62302786a95a4a5e9c05356
Toporivskaya.A <tprkvakvin2@gmail.com>
2026-04-25 09:56:16 +0300

    Delete Task2 directory

--- Task2/ThreadOperationException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ThreadErrorApp
-{
-    public class ThreadOperationException : Exception
-    {
-        public ThreadOperationException(string message, Exception innerException)
-            : base(message, innerException) { }
-    }
-}
