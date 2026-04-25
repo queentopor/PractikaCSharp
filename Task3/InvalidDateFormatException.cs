d6c02e7ece1634583bf462e455a19ddf55e22514
Toporivskaya.A <tprkvakvin2@gmail.com>
2026-04-25 09:56:32 +0300

    Delete Task3 directory

--- Task3/InvalidDateFormatException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DateValidationApp
-{
-    public class InvalidDateFormatException : Exception
-    {
-        public InvalidDateFormatException() : base("Некорректный формат даты.") { }
-        public InvalidDateFormatException(string message) : base(message) { }
-        public InvalidDateFormatException(string message, Exception inner) : base(message, inner) { }
-    }
-}
